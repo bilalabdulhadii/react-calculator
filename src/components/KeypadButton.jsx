85a2d4cdf9a962de14cd4aa07e8a835837ee0d1a
bilalabdulhadii <bilalabdulhadipro@gmail.com>
2025-08-20 08:47:24 +0300

    Basic Calculator setup

---
+++ src/components/KeypadButton.jsx
@@ -0,0 +1,20 @@
+import { Button } from "@mui/material";
+
+export default function KeypadNumberButton({ text, onClick }) {
+    return (
+        <Button
+            className={"calcBtn"}
+            variant="contained"
+            disableRipple
+            sx={{
+                width: "100%",
+                height: "52px",
+                padding: 0,
+                minWidth: 0,
+            }}
+            onClick={onClick}
+        >
+            <span className="buttonSpan">{text}</span>
+        </Button>
+    );
+}
